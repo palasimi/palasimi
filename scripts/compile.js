11ba3b3c8e4806eed5d53929fee8041df80095b0
Levi Gruspe <mail.levig@gmail.com>
2023-07-03 10:26:55 +0800

    Compile README.md using marked.js instead of pandoc

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,16 +1,21 @@
-const { existsSync, writeFileSync } = require("fs");
+const { existsSync, readFileSync, writeFileSync } = require("fs");
 const { chdir } = require("process");
 
+const { marked } = require("marked");
 const { configure, render } = require("nunjucks");
 
 while (!existsSync("package.json")) {
   chdir("..");
 }
 
+// Compile README.md.
+const readme = readFileSync("README.md", { encoding: "utf8" });
+const article = marked.parse(readme, { mangle: false, headerIds: false });
+
 // Generate index.html and about.html.
 configure("templates");
 
-const about = render("about.html");
+const about = render("about.html", { article });
 writeFileSync("dist/index.html", about);
 writeFileSync("dist/about.html", about);
 writeFileSync("dist/graph.html", render("graph.html"));
